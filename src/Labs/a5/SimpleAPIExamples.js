d2a384e7ad1561c3fcb6a5be4c682cc1444f1c31
benjaminlau-12 <lau.be@northeastern.edu>
2023-11-14 00:27:25 -0500

    Finished until 3.4

---
+++ src/Labs/a5/SimpleAPIExamples.js
@@ -0,0 +1,15 @@
+import EncodingParametersInURLs from "./EncodingParametersInUrls";
+import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithObjects from "./WorkingWithObjects";
+
+function SimpleAPIExamples() {
+    return (
+        <div>
+            <h3>Simple API Examples</h3>
+            <EncodingParametersInURLs/>
+            <WorkingWithObjects/>
+            <WorkingWithArrays/>
+        </div>
+    );
+}
+export default SimpleAPIExamples;
